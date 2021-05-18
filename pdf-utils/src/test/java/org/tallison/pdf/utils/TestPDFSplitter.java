db461039ef01975e7b394eb6df44f55002147180
tballison <tallison@apache.org>
2021-05-18 14:16:57 -0400

    ignore test...doh!

--- pdf-utils/src/test/java/org/tallison/pdf/utils/TestPDFSplitter.java
+++ pdf-utils/src/test/java/org/tallison/pdf/utils/TestPDFSplitter.java
@@ -4,12 +4,14 @@ package org.tallison.pdf.utils;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 
 public class TestPDFSplitter {
 
     @Test
+    @Ignore
     public void testSimple() throws Exception {
 
         PDFSplitter.main(new String[]{
