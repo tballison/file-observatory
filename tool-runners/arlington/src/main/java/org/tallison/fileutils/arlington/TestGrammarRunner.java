3c39815437b7d80505ee5d61a93809f3e78fcff1
tballison <tallison@apache.org>
2021-09-23 10:16:37 -0400

    small improvements throughout

--- tool-runners/arlington/src/main/java/org/tallison/fileutils/arlington/TestGrammarRunner.java
+++ tool-runners/arlington/src/main/java/org/tallison/fileutils/arlington/TestGrammarRunner.java
@@ -104,7 +104,7 @@ public class TestGrammarRunner extends AbstractDirectoryProcessor {
     public static void main(String[] args) throws Exception {
 
         TestGrammarRunner runner = new TestGrammarRunner(
-                ConfigSrc.build(args, getName(), 1, MAX_BUFFER));
+                ConfigSrc.build(args, getName(), MAX_BUFFER, MAX_BUFFER));
 
         runner.execute();
     }
